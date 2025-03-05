52e0e8cb831e100e571f6d0712d2e4f38ec94d73
Yiwen Chan <133894629+v41827@users.noreply.github.com>
2025-03-05 20:36:51 +0000

    Update train.sh with student info

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-STUDENT_ID=kn00794 STUDENT_NAME="Jane Doe" python main.py \
+STUDENT_ID=yc01847 STUDENT_NAME="Yiwen Chan" python main.py \
 -s veri \
 -t veri \
 -a mobilenet_v3_small \
